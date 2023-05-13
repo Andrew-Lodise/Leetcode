d332051974397da3e64a8cdc3a16a9edb4028288
Andrew-Lodise <AL964440@wcupa.edu>
2023-05-13 08:14:06 -0400

    setup for first question

---
+++ contains_duplicate.py
@@ -0,0 +1,9 @@
+from typing import List
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+      return True
+    
+
+mysolution = Solution()
+print(mysolution.containsDuplicate([1,2,3,4]))
