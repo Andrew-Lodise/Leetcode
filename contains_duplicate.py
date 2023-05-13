ae1f5ae4a1e60be0f939c8f6462b7a14e7a754a2
Andrew-Lodise <AL964440@wcupa.edu>
2023-05-13 08:49:03 -0400

    first problem

--- contains_duplicate.py
+++ contains_duplicate.py
@@ -1,9 +1,22 @@
 from typing import List
 
+'''
+Given an integer array nums, 
+return true if any value appears at least twice in the array, 
+and return false if every element is distinct.
+'''
+
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-      return True
+      dict = {}
+      for n in nums:
+        if n not in dict:
+          dict[n] = 1
+        else:
+          dict[n] += 1
+
+      return (any(n>1 for n in dict.values()))
     
 
 mysolution = Solution()
-print(mysolution.containsDuplicate([1,2,3,4]))
+print(mysolution.containsDuplicate([1,2,3,4,5,5]))
